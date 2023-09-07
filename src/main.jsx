83ddcec7057525e64d9f6f62c850d0a0a832357f
Gael González <gaeldev032@gmail.com>
2023-09-06 19:28:32 -0700

    🌎 Adding i18next

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { LangProvider } from "./components/LangProvider/LangProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <LangProvider>
+      <App />
+    </LangProvider>
   </React.StrictMode>
 );
