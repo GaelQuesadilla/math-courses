c95033e3b410b60856ebaae31862e563b82ee9df
Gael González <gaeldev032@gmail.com>
2023-09-13 22:10:49 -0700

    Add: CourseCard and courses layout

---
+++ src/components/Courses/CourseCard.jsx
@@ -0,0 +1,22 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+
+export const CourseCard = ({ children, title, to, ...props }) => {
+  const [t, i18n] = useTranslation("global");
+  return (
+    <>
+      <Card className="my-4 mx-auto" border="primary">
+        <Card.Header as="h5">Curso</Card.Header>
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{children}</Card.Text>
+          <Button as={Link} to={"/course/" + to} variant="primary">
+            {t("Courses.link-btn")}
+          </Button>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
---
+++ src/courses/courses.js
@@ -0,0 +1,10 @@
+export const courses = {
+  "Mathematical-language": {
+    title: "Courses.Mathematical-language.title",
+    description: "Courses.Mathematical-language.description",
+  },
+  "Operations-hierarchy": {
+    title: "Courses.Operations-hierarchy.title",
+    description: "Courses.Operations-hierarchy.description",
+  },
+};
