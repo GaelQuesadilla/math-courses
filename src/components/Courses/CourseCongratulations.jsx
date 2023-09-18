a8269f8f954f121d6f01fb1a507382785ea0235a
Gael González <gaeldev032@gmail.com>
2023-09-17 18:48:00 -0700

    🎉 Adding congratulations page

--- src/components/Courses/CourseCongratulations.jsx
+++ src/components/Courses/CourseCongratulations.jsx
@@ -1,5 +1,26 @@
-import { Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+import useWindowSize from "react-use/lib/useWindowSize";
+import Confetti from "react-confetti";
+import { ROUTES } from "../../constants/ROUTES";
 
 export const CourseCongratulations = () => {
-  return <Container>Felicidades</Container>;
+  const [t, i18n] = useTranslation("global");
+  const { width, height } = useWindowSize();
+  return (
+    <>
+      <Container>
+        <Confetti width={width} height={height} />
+        <h2 className="my-3">{t("Courses.congratulations")}</h2>
+        <h3 className="my-3">{t("Courses.finished-course")}</h3>
+
+        <p>{t("Courses.congratulation-message")}</p>
+
+        <Button as={Link} to={ROUTES.INDEX.href}>
+          {t("Courses.return-btn")}
+        </Button>
+      </Container>
+    </>
+  );
 };
