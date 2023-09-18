31d3209348a52a741ab9e759c7368547fcc6898d
Gael González <gaeldev032@gmail.com>
2023-09-17 18:57:34 -0700

    🔧 Fixing routes

--- src/constants/ROUTES.jsx
+++ src/constants/ROUTES.jsx
@@ -20,4 +20,21 @@ export const ROUTES = {
       />
     ),
   },
+
+  COURSE_PREVIEW: {
+    href: "/preview/:courseName",
+    getHref: (course) => {
+      return `/preview/${course}`;
+    },
+    name: "Routes.preview",
+  },
+
+  COURSE: {
+    href: "/course/:courseName",
+    getHref: (course) => {
+      return `/course/${course}`;
+    },
+
+    name: "Routes.course",
+  },
 };
